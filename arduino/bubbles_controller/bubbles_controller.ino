ec09425c07196269ecea487dc7bd54ffab58b15c
theleadingzero <becky@theleadingzero.com>
2014-02-09 12:43:28 +0000

    Testing communication between microcontroller and linino

--- arduino/bubbles_controller/bubbles_controller.ino
+++ arduino/bubbles_controller/bubbles_controller.ino
@@ -1,11 +1,24 @@
 #include <Process.h>
 
-void setup() {
-  // put your setup code here, to run once:
+Process twitter;     // process to read tweets
 
+void setup() {
+  Bridge.begin();    // initialise Bridge
+  Serial.begin(9600);   // initialise serial
+
+  while (!Serial);  // wait for Serial Monitor to open
+  Serial.println( "Welcome to the BubblinO2" );
+
+  // run the Python script on the Linino
+  twitter.runShellCommend( "python /usr/bubblino2/linino/tweet-controller.py" );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read command output
+  while (twitter.available()) {
+    int result = twitter.parseInt();			// look for an integer
+    Serial.println(result);			// print the number as well
+  }
+  delay(5000);  // wait 5 seconds before you do it again
 
 }
